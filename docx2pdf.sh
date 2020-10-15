914270078ea2e29bc50e5ef265c86fc5e846ffee
Masumi Morishige <masumi.developer@gmail.com>
2020-10-15 20:15:10 +0900

    Fixed: Shell file

--- docx2pdf.sh
+++ docx2pdf.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+argnum=$#
+if [ $argnum = 0 ]; then
+    echo "Converting every file to pdf..."
+    python3 docx2pdf.py
+elif [ $argnum = 1 ]; then
+    echo "Converting $1 to pdf..."
+    python3 docx2pdf.py $1
+else
+    echo "Error: It can only pass one argument."
+    exit 0 
+fi
 
+echo "Converted Successfully!"
 exit 0
